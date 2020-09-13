8842aa6718449a8ca7db03d674bfa71c0ad32126
Jhony Angulo <jhony.angulo@utec.edu.pe>
2020-09-13 15:48:41 -0500

    Plot results

--- Hill Climbing/plotting.py
+++ Hill Climbing/plotting.py
@@ -1,13 +1,34 @@
 import matplotlib.pyplot as plt
 
-data = {'Naive': 10, 'Hill climbing': 15}
-x_axis = ['naive', 'Hill climbing']
+file = open('data.txt')
+time, *data = map(lambda el: el.split() , file.readlines())
+time = " ".join(time)
 
-names = list(data.keys())
-values = list(data.values())
+x_axis = []
+y_axis = []
+
+for x,y in data:
+    x_axis.append(int(x))
+    y_axis.append(int(y))
 
 fig, axs = plt.subplots()
-axs.bar(names, values)
-fig.suptitle('Permormance algorithm')
+axs.bar(x_axis, y_axis)
+fig.suptitle('Results\n'+time)
+
+axs.ticklabel_format(style='plain',useOffset=False)
+axs.set_yscale('log')
+axs.set_ylabel('Frecuencia')
+axs.set_xlabel('Min. score')
+
+for x,y in zip(x_axis,y_axis):
+    
+
+    label = "{:d}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0, 2), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
 
 plt.show()
