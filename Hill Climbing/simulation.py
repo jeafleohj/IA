86d8bddab1cbfe64bb40112414d3924dc72007bd
TISparta <jose.garcia@utec.edu.pe>
2020-09-13 13:41:49 -0500

    adding progress bar

--- Hill Climbing/simulation.py
+++ Hill Climbing/simulation.py
@@ -1,15 +1,19 @@
 from sys import setrecursionlimit
 from hillclimbing import ChessBoard
 import timeit
+from sys import stdout
 
 setrecursionlimit(int(1e9))
 N = 8
 queens = []
 score = dict()
 max_time = 0
+it = 0
+total = N**N
 
 def backtrack ():
     global max_time
+    global it
     if len(queens) == N:
         chess = ChessBoard(queens)
         chess.hillclimbing()
@@ -21,12 +25,16 @@ def backtrack ():
             score[cur_score] = 1
         else:
             score[cur_score] += 1
+        it += 1
+        stdout.write('Process = %.2f%%  \r' %(100.0 * it / total))
+        stdout.flush()
         return
     for r in range(N):
         queens.append(r)
         backtrack()
         queens.pop()
 backtrack()
-print("Max_time: %.6f" %(max_time))
-for score, frecuency in score.items():
-    print(score, frecuency)
+with open('data.txt', 'w') as file:
+    file.write('Max_time: %.6f mili seconds\n' %(max_time))
+    for score, frecuency in score.items():
+        file.write('%d %d\n' %(score, frecuency))
